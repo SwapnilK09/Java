e2c072d092c08cb32dd4594a1f272c3952870f53
SwapnilK09 <swapnilkadam9002@gmail.com>
2025-07-08 05:52:45 +0000

    Added

---
+++ Basics/Arr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Arr{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                System.out.println("Enter the array size : ");
+                int size = sc.nextInt();
+                int[] arr = new int[size];
+        System.out.println("Enter the array elements : ");
+        for(int i=0;i<size;i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.err.println("Entered numbers are : ");
+        for(int num : arr){
+            System.out.println(num + "");
+        }
+    }
+}
